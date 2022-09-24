f8dc3d218f48a72920a72d5f9e6311cb918dc2c9
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-09-25 04:20:37 +0800

    Get character RGB gap.

--- char_occurence/src/main.rs
+++ char_occurence/src/main.rs
@@ -5,6 +5,9 @@ fn main() {
     let mut args = env::args();
     args.next();
     let string = args.collect::<String>();
+
+    let occurences = get_occurences(&string);
+    occurences.iter().count();
 }
 
 fn get_occurences(string: &str) -> HashMap<char, u8> {
@@ -15,6 +18,31 @@ fn get_occurences(string: &str) -> HashMap<char, u8> {
     }
 
     occurences
+}
+
+fn get_char_rgb_gap(unique_char: usize) -> Option<[u8; 3]> {
+    if unique_char > 256 * 3 {
+        None
+    } else {
+        let rgb_len = {
+            let r = unique_char % 3;
+            let g = unique_char / 3;
+            let b = unique_char / 9;
+
+            [r, g, b]
+        };
+        let mut rgb_gap = [0u8; 3];
+
+        for i in 0..3 {
+            // "Inflate" RGB to fill u8.
+            let c = rgb_len[i];
+            if c != 0 {
+                rgb_gap[i] += 255 / c as u8
+            };
+        }
+
+        Some(rgb_gap)
+    }
 }
 
 #[cfg(test)]
@@ -45,5 +73,23 @@ mod tests {
     #[test]
     fn occurences_chinese() {
         assert_eq!(get_occurences("习习白習卒翠")[&'习'], 2);
+    }
+
+    #[test]
+    fn red_rgb_gap() {
+        assert_eq!(get_char_rgb_gap(1).expect("1 <= 256 * 3"), [255, 0, 0]);
+    }
+
+    #[test]
+    fn middle_char_rgb_gap() {
+        assert_eq!(
+            get_char_rgb_gap(128 * 3).expect("128 * 3 <= 256 * 3"),
+            [0, 1, 6]
+        );
+    }
+
+    #[test]
+    fn too_many_unique_char_rgb_gap() {
+        assert_eq!(get_char_rgb_gap(256 * 3 + 1), None);
     }
 }
