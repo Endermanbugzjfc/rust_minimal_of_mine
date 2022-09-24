bff4bbd9a43c0b2ca1ade56dae5df754cad875fc
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-09-25 05:08:17 +0800

    Terminal colouring is too complicated.

--- char_occurence/src/main.rs
+++ char_occurence/src/main.rs
@@ -1,5 +1,5 @@
-use colored::*;
 use std::{collections::HashMap, env};
+static SUPERSCRIPT_NUMBERS: [char; 10] = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
 
 fn main() {
     let mut args = env::args();
@@ -7,42 +7,29 @@ fn main() {
     let string = args.collect::<String>();
 
     let occurences = get_occurences(&string);
-    occurences.iter().count();
+
+    println!(
+        "{}",
+        string
+            .chars()
+            .map(|c| {
+                let mut string = c.to_string();
+                string.push(SUPERSCRIPT_NUMBERS[occurences[&c]]);
+
+                string
+            })
+            .collect::<String>()
+    );
 }
 
-fn get_occurences(string: &str) -> HashMap<char, u8> {
-    let mut occurences: HashMap<char, u8> = HashMap::new();
+fn get_occurences(string: &str) -> HashMap<char, usize> {
+    let mut occurences: HashMap<char, usize> = HashMap::new();
     for c in string.chars() {
         let entry = occurences.entry(c).or_insert(0);
         *entry += 1
     }
 
     occurences
-}
-
-fn get_char_rgb_gap(unique_char: usize) -> Option<[u8; 3]> {
-    if unique_char > 256 * 3 {
-        None
-    } else {
-        let rgb_len = {
-            let r = unique_char % 3;
-            let g = unique_char / 3;
-            let b = unique_char / 9;
-
-            [r, g, b]
-        };
-        let mut rgb_gap = [0u8; 3];
-
-        for i in 0..3 {
-            // "Inflate" RGB to fill u8.
-            let c = rgb_len[i];
-            if c != 0 {
-                rgb_gap[i] += 255 / c as u8
-            };
-        }
-
-        Some(rgb_gap)
-    }
 }
 
 #[cfg(test)]
@@ -56,7 +43,7 @@ mod tests {
 
     #[test]
     fn unique_occurences_ascii() {
-        assert_eq!(get_occurences("Mienraft").values().sum::<u8>(), 8);
+        assert_eq!(get_occurences("Mienraft").values().sum::<usize>(), 8);
         // pls vote 4 https://gist.github.com/PEMapModder/cb60bd61b5f5d08ae3dc81781feb8449?permalink_comment_id=3900366#gistcomment-3900366
     }
 
@@ -67,29 +54,11 @@ mod tests {
 
     #[test]
     fn unique_occurences_chinese() {
-        assert_eq!(get_occurences("又双叒叕").values().sum::<u8>(), 4);
+        assert_eq!(get_occurences("又双叒叕").values().sum::<usize>(), 4);
     }
 
     #[test]
     fn occurences_chinese() {
         assert_eq!(get_occurences("习习白習卒翠")[&'习'], 2);
-    }
-
-    #[test]
-    fn red_rgb_gap() {
-        assert_eq!(get_char_rgb_gap(1).expect("1 <= 256 * 3"), [255, 0, 0]);
-    }
-
-    #[test]
-    fn middle_char_rgb_gap() {
-        assert_eq!(
-            get_char_rgb_gap(128 * 3).expect("128 * 3 <= 256 * 3"),
-            [0, 1, 6]
-        );
-    }
-
-    #[test]
-    fn too_many_unique_char_rgb_gap() {
-        assert_eq!(get_char_rgb_gap(256 * 3 + 1), None);
     }
 }
