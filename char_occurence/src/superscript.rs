99c6e7725ba116af32854e52bb1b571e2a4f680f
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2022-09-25 21:27:14 +0800

    Superscript module.

--- char_occurence/src/superscript.rs
+++ char_occurence/src/superscript.rs
@@ -2,13 +2,26 @@ pub trait Superscript {
     fn superscript(&self) -> String;
 }
 
-pub static NUMBERS: [char; 10] = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
+static NUMBERS: [char; 10] = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
 
-impl Superscript for u8 {
+impl Superscript for usize {
     fn superscript(&self) -> String {
-        self.to_string()
-            .chars()
-            .map(|c| superscript_digit(c))
-            .collect()
+        let n = *self;
+        if n > NUMBERS.len() {
+            (n / 10).superscript() + &(n % 10).superscript()
+        } else {
+            NUMBERS[n].to_string()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn u32_max() {
+        let n = u32::MAX as usize;
+        assert_eq!(n.superscript(), "⁴²⁹⁴⁹⁶⁷²⁹⁵");
     }
 }
